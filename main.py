1e6c3bb25c716f2b28af9821aebc4e6e5c499d0d
tansylu <tansiluakh@gmail.com>
2023-07-31 11:04:32 +0200

    Step 2: training with RandomForestClassifier

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from imblearn.over_sampling import SMOTE
 from sklearn.ensemble import RandomForestClassifier
 
 # Specify the path to your CSV file
-csv_file_path = "gym_data (2).csv"
+csv_file_path = "gym_data.csv"
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv(csv_file_path)
@@ -25,66 +25,66 @@ numerical = cols
 # # and friend promo/partner company are negligible for being 50/50
 
 # # Create a directory for saving boxplot images
-boxplot_dir = "boxplot_images"
-if not os.path.exists(boxplot_dir):
-    os.makedirs(boxplot_dir)
+# boxplot_dir = "boxplot_images"
+# if not os.path.exists(boxplot_dir):
+#     os.makedirs(boxplot_dir)
 
 # # Create a directory for saving histogram images
-histogram_dir = "histogram_images"
-if not os.path.exists(histogram_dir):
-    os.makedirs(histogram_dir)
+# histogram_dir = "histogram_images"
+# if not os.path.exists(histogram_dir):
+#     os.makedirs(histogram_dir)
 
 # # Generate histograms for each variable and save them as images
-for col in numerical:
-    plt.figure(figsize=(8, 6))
-    sns.histplot(df[col])
-    plt.title(f"{col} Histogram")
-    plt.savefig(os.path.join(histogram_dir, f"{col}_histogram.png"))  # Save the histogram as an image
-    plt.close()  # Close the plot to release memory
+# for col in numerical:
+#     plt.figure(figsize=(8, 6))
+#     sns.histplot(df[col])
+#     plt.title(f"{col} Histogram")
+#     plt.savefig(os.path.join(histogram_dir, f"{col}_histogram.png"))  # Save the histogram as an image
+#     plt.close()  # Close the plot to release memory
 
 # # Calculate the correlation matrix
-correlation_matrix = df.corr()
+# correlation_matrix = df.corr()
 
-# # Plot the correlation matrix
-plt.figure(figsize=(12, 10))
-sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
-plt.title("Correlation Matrix")
-plt.savefig("correlation_matrix.png")  # Save the correlation matrix plot
-plt.show()
+# # # Plot the correlation matrix
+# plt.figure(figsize=(12, 10))
+# sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
+# plt.title("Correlation Matrix")
+# plt.savefig("correlation_matrix.png")  # Save the correlation matrix plot
+# plt.show()
 
 # # Based on the correlation matrix, we can see that Age and Cancellation frequency contribute
 # # towards a higher chance of a person exiting, while high additional charges and weekly
 # # class registration indicate a client not exiting
 
 # # Generate box plots for each variable against "Exited" and save them as images
-for col in df.columns:
-    if col != "Exited":  # Exclude the "Exited" variable itself
-        plt.figure(figsize=(8, 6))
-        sns.boxplot(x="Exited", y=col, data=df)
-        plt.title(f"{col} Boxplot")
-        plt.savefig(os.path.join(boxplot_dir, f"{col}_boxplot.png"))  # Save the boxplot as an image
-        plt.close()  # Close the plot to release memory
+# for col in df.columns:
+#     if col != "Exited":  # Exclude the "Exited" variable itself
+#         plt.figure(figsize=(8, 6))
+#         sns.boxplot(x="Exited", y=col, data=df)
+#         plt.title(f"{col} Boxplot")
+#         plt.savefig(os.path.join(boxplot_dir, f"{col}_boxplot.png"))  # Save the boxplot as an image
+#         plt.close()  # Close the plot to release memory
 
 # ##Contract period, partner company, registration and friend promo can be omitted as show no diffference
 
 
 # # Create a directory for saving sns plots
-sns_dir = "sns"
-if not os.path.exists(sns_dir):
-    os.makedirs(sns_dir)
+# sns_dir = "sns"
+# if not os.path.exists(sns_dir):
+#     os.makedirs(sns_dir)
 
 # #Make some count plots for exited and staying customers description
-cols = ['Cancellation_freq', "Avg_additional_charges_total", "Class_registration_weekly", "Contract_period"]
+# cols = ['Cancellation_freq', "Avg_additional_charges_total", "Class_registration_weekly", "Contract_period"]
 
-for col in cols:
-    plt.figure(figsize=(14, 4))
-    ax = sns.countplot(x="Exited", huea=str(col), data=df)
-    plt.title(col)
-    ax.locator_params(axis='x', nbins=5)  # Adjust the number of x-axis ticks
+# for col in cols:
+#     plt.figure(figsize=(14, 4))
+#     ax = sns.countplot(x="Exited", hue=str(col), data=df)
+#     plt.title(col)
+#     ax.locator_params(axis='x', nbins=5)  # Adjust the number of x-axis ticks
     
-    plt.title(col)
-    plt.savefig(os.path.join(sns_dir, f"{col}_countplot.png"))
-    plt.close()
+#     plt.title(col)
+#     plt.savefig(os.path.join(sns_dir, f"{col}_countplot.png"))
+#     plt.close()
 
 # #Data preprocessing:
 
@@ -113,3 +113,12 @@ X_train, y_train = X_smote, y_smote
 
 print(y_train.value_counts())
 
+from sklearn.ensemble import RandomForestClassifier
+
+rf = RandomForestClassifier(random_state=46)
+rf.fit(X_train,y_train)
+
+from sklearn.metrics import accuracy_score
+
+preds = rf.predict(X_test)
+print(accuracy_score(preds,y_test))
